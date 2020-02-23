31dcc184272e708fa8bbce243e22e1a27ff72a2a
Curro Casado <currocuo9625@gmail.com>
2020-02-22 19:57:22 -0600

    Add a new <p> on second page, delete the image on index page, and change color of Header background

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#80ced6`,
       marginBottom: `1.45rem`,
     }}
   >
