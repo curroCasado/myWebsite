a37065299270f1b35b6bed6d2428bd43839728b2
Curro Casado <currocuo9625@gmail.com>
2020-03-02 18:53:25 -0600

    Footer update:
    Add contact Info and recolocate on the index.js.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,60 @@ import React from "react"
 const Footer = () => {
   return (
     <>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a> By curro casado
+      <footer style={{ background: `#00001a` }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <a
+            href=""
+            style={{
+              color: `#803535`,
+              paddingLeft: 10,
+              paddingRight: 10,
+            }}
+          >
+            LinkedIn
+          </a>
+          <a
+            href=""
+            style={{
+              color: `#803535`,
+              paddingLeft: 10,
+              paddingRight: 10,
+            }}
+          >
+            GitHub
+          </a>
+          <a
+            href=""
+            style={{
+              color: `#803535`,
+              paddingLeft: 10,
+              paddingRight: 10,
+            }}
+          >
+            Facebook
+          </a>
+        </div>
+        <div
+          style={{
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <p>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org" style={{ color: `#803535` }}>
+              Gatsby
+            </a>{" "}
+            By Curro Casado
+          </p>
+        </div>
       </footer>
     </>
   )
